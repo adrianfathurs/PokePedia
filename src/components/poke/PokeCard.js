f682ffedfd586e679f16112dfd290ac2b46ebc7f
Adrian Fathur Setyawan <fatursetiawan80@gmail.com>
2022-04-11 22:42:10 +0700

    feat(PokeCard): add pokecard to detailing pokemon

---
+++ src/components/poke/PokeCard.js
@@ -0,0 +1,63 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Carousel from 'react-bootstrap/Carousel'
+import {connect} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+
+import '../../assets/css/PokeCard.css'
+import {
+  Card,
+  CardHeader,
+  Button,
+} from "shards-react";
+
+const PokeCard = ( {pokemon} ) => {
+  
+  console.log(pokemon)
+  let data = (pokemon.sprites || {});
+  let pokemonSprites = [
+    data.back_default,
+    data.back_female,
+    data.back_shiny,
+    data.back_shiny_female,
+    data.front_default,
+    data.front_female,
+    data.front_shiny,
+    data.front_shiny_female, 
+  ]
+  let modal = useSelector((state) => state.modal)
+  console.log(modal,"ini state")
+  const dispatch = useDispatch()
+  return(
+  <Card small className="mb-4 pt-3">
+    <CardHeader className="border-bottom text-center">
+      <Carousel className="mb-2">
+        {pokemonSprites.map((pokeSprites, index) => ( 
+          <Carousel.Item key={index}>
+            <img
+              className="d-block w-100"
+              src={pokeSprites ? pokeSprites : "https://dummyimage.com/600x600/000/5f63a3" }
+              fluid="true"
+              />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      <Button pill outline size="sm" onClick={()=>dispatch({type:'HANDLE_MODAL_TOOLTIP', value:true})} className="mb-2">
+        <i className="material-icons mr-1">catching_pokemon</i> Catch Pokemon
+      </Button>
+      
+    </CardHeader>
+  </Card>
+)}
+
+const mapStateToProps = (state)=> {
+  return {
+    modal: state.modal
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleCatchPoke: () => dispatch({type:'HANDLE_MODAL'}),
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(PokeCard);
