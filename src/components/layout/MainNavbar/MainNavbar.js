6c6a03644f9c8e78f303edd6e4067f014a342a74
Adrian Fathur Setyawan <fatursetiawan80@gmail.com>
2022-04-14 09:47:23 +0700

    feat(MainNavbar): remove unused search field on main navbar

--- src/components/layout/MainNavbar/MainNavbar.js
+++ src/components/layout/MainNavbar/MainNavbar.js
@@ -2,8 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import { Container, Navbar } from "shards-react";
-
-import NavbarSearch from "./NavbarSearch";
 import NavbarNav from "./NavbarNav/NavbarNav";
 import NavbarToggle from "./NavbarToggle";
 
@@ -18,7 +16,6 @@ const MainNavbar = ({ layout, stickyTop }) => {
     <div className={classes}>
       <Container className="p-0">
         <Navbar type="light" className="align-items-stretch flex-md-nowrap p-0">
-          <NavbarSearch />
           <NavbarNav />
           <NavbarToggle />
         </Navbar>
