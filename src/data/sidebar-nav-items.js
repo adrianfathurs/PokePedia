e6b12c8ccd3c68683c33584b51312d000961157a
Adrian Fathur Setyawan <fatursetiawan80@gmail.com>
2022-04-14 09:54:56 +0700

    refactor(sidebar-nav-items): remove property unused from template

--- src/data/sidebar-nav-items.js
+++ src/data/sidebar-nav-items.js
@@ -2,7 +2,7 @@ export default function() {
   return [
     {
       title: "All Pokemon List",
-      htmlBefore: '<i class="material-icons">vertical_split</i>',
+      htmlBefore: '<i class="material-icons">note_add</i>',
       to: "/pokemon-list",
     },
     {
@@ -10,41 +10,5 @@ export default function() {
       htmlBefore: '<i class="material-icons">vertical_split</i>',
       to: "/my-pokemon-list",
     },
-    {
-      title: "Blog Dashboard",
-      to: "/blog-overview",
-      htmlBefore: '<i class="material-icons">edit</i>',
-      htmlAfter: ""
-    },
-    {
-      title: "Blog Posts",
-      htmlBefore: '<i class="material-icons">vertical_split</i>',
-      to: "/blog-posts",
-    },
-    {
-      title: "Add New Post",
-      htmlBefore: '<i class="material-icons">note_add</i>',
-      to: "/add-new-post",
-    },
-    {
-      title: "Forms & Components",
-      htmlBefore: '<i class="material-icons">view_module</i>',
-      to: "/components-overview",
-    },
-    {
-      title: "Tables",
-      htmlBefore: '<i class="material-icons">table_chart</i>',
-      to: "/tables",
-    },
-    {
-      title: "User Profile",
-      htmlBefore: '<i class="material-icons">person</i>',
-      to: "/user-profile-lite",
-    },
-    {
-      title: "Errors",
-      htmlBefore: '<i class="material-icons">error</i>',
-      to: "/errors",
-    }
   ];
 }
